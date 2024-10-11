c07aefc9a093f6f1cf506a897acb33f4a1c99fd8
byllet <1rasim1@list.ru>
2024-10-11 13:33:17 +0300

    fixed compose, added constraits for handlers

--- backend/src/backend.py
+++ backend/src/backend.py
@@ -1,16 +1,31 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response
 
 
 app = Flask(__name__)
 
 
-@app.route('/search', methods=['POST'])
-def search():
-    text = request.json['text']
-    return jsonify({'text' : text})
+def incorrect_params(param):
+    return make_response(f'Not found in json: {param}\n', 400)
+
+
+@app.route('/searching', methods=['POST'])
+def searching():
+    if 'text' not in request.json:
+        return incorrect_params('text')
+    if 'top_k' not in request.json:
+        return incorrect_params('top_k') 
+    
+    return jsonify({'text' : request.json['text'],  
+                    'top_k' : request.json['top_k']})
 
 
 @app.route('/indexing', methods=['POST'])
 def indexing():
-    text = request.json['text']
-    return jsonify({'text' : text})
+    if 'dataset_name_of_docs' not in request.json:
+         return incorrect_params('dataset_name_of_docs')
+    
+    return request.json['dataset_name_of_docs'][0]['content']
+
+
+if __name__ == '__main__':
+    app.run(host='::', port='5000')
