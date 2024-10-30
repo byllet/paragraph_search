b2c5698f508062ffe45f7710b692eaa47f06f96f
byllet <1rasim1@list.ru>
2024-10-30 21:16:40 +0300

    feat: added qdrant ann search

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # paragraph_search
+
+ - Что нужно для запуска?   
+    1) Должен быть установлен докер
+
+    2)  ```
+        paragraph_search$ docker compose up
+        ```
+
+- Примеры запросов:
+
+    - Индексация данных 
+    ```
+    curl -X POST localhost:5000/indexing \
+    -H "Content-Type: application/json" \
+    -d '{"dataset_name_of_docs": [{"content" : "I sat on the bank of the river today."},
+                                {"content" : "Я сидел у реки весь день."},
+                                {"content" : "Первым в этот день дежурил Сэм, но по собственной воле к нему присоединился Арагорн. Остальные один за другим уснули. Над лощиной стремительно стала расти тишина. Ее почувствовал даже Сэм "}]}'
+    ```
+
+    - Поиск по параграфам
+
+    ```
+    curl -X POST localhost:5000/search \
+    -H "Content-Type: application/json" \
+    -d '{"text": "some very interesting querry", "top_k": 100}'
+    ```
--- backend/src/main.py
+++ backend/src/main.py
@@ -1,10 +1,11 @@
 from flask import Flask, jsonify, request, make_response
+import json
 
-from paragraphs import indexing_data
-from find import find_top_k
+from searcher import Searcher
 
 
 app = Flask(__name__)
+searcher = Searcher(collection_name='paragraphs')
 
 
 def incorrect_params(param):
@@ -18,7 +19,7 @@ def searching():
     if 'top_k' not in request.json:
         return incorrect_params('top_k') 
     
-    return find_top_k(text=request.json['text'], top_k=request.json['top_k'])
+    return json.dumps(searcher.search(text=request.json['text'], top_k=request.json['top_k']), ensure_ascii=False)
 
 
 @app.route('/indexing', methods=['POST'])
@@ -26,11 +27,8 @@ def indexing():
     if 'dataset_name_of_docs' not in request.json:
          return incorrect_params('dataset_name_of_docs')
     
-    for doc in request.json['dataset_name_of_docs']:
-        if 'content' not in doc:
-            return incorrect_params('content')
-
-        indexing_data(doc['content'])
+    if searcher.index(request.json['dataset_name_of_docs']) == -1:
+        return incorrect_params('content') 
     
     return jsonify(success=True)
 
